2ce5c79c9472025421a1ab939e3df8c142616981
matyliano <joanna.boszko@gmail.com>
2019-05-23 19:40:52 +0200

    basic classes

---
+++ src/main/java/matyliano/exchange/controller/ExchangeController.java
@@ -0,0 +1,20 @@
+package matyliano.exchange.controller;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ExchangeController {
+
+    public ModelAndView showExchangeRate(@RequestParam String exchangeRateFrom,
+                                         @RequestParam String exchangeRateTo) {
+
+        //exchange rate
+
+
+        //historical rate
+
+
+    }
+}
--- src/main/java/matyliano/exchange/model/History.java
+++ src/main/java/matyliano/exchange/model/History.java
@@ -1,10 +1,61 @@
 package matyliano.exchange.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class History {
 
+    String fromCurrency;
+    String toCurrency;
+    String rate;
+    String date;
+    String open;
+    String high;
+    String low;
+    String close;
 
+    public History(String fromCurrency, String toCurrency, String rate, String date, String open, String high, String low, String close) {
+        this.fromCurrency = fromCurrency;
+        this.toCurrency = toCurrency;
+        this.rate = rate;
+        this.date = date;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+    }
 
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public String getClose() {
+        return close;
+    }
 }
