b56c3435fc05ec2d158aefc211c9f1850241a3b6
matyliano <joanna.boszko@gmail.com>
2019-05-26 13:42:30 +0200

    an embarrassing mistake fixed

--- src/main/java/matyliano/exchange/model/History.java
+++ src/main/java/matyliano/exchange/model/History.java
@@ -4,17 +4,16 @@ import lombok.Data;
 import lombok.ToString;
 
 @Data
-@ToString
 public class History {
 
-    String fromCurrency;
-    String toCurrency;
-    String rate;
-    String date;
-    String open;
-    String high;
-    String low;
-    String close;
+    private String fromCurrency;
+    private String toCurrency;
+    private String rate;
+    private String date;
+    private String open;
+    private String high;
+    private String low;
+    private String close;
 
     public History(String fromCurrency, String toCurrency, String rate, String date, String open, String high, String low, String close) {
         this.fromCurrency = fromCurrency;
